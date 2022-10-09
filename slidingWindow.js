578ec5f6e5e225a9cb1dbcb652f6984c7d6a59c8
slmnkankal <slmnknkl@gmail.com>
2022-10-09 21:12:45 +0300

    review

--- slidingWindow.js
+++ slidingWindow.js
@@ -1,29 +1,28 @@
 //Sliging Window Algorithm
 
 function maxSumArr(arr, num) {
-    let maxSum = 0;
-    let tempSum = 0;
-    if(arr.length < num) return null;
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
 
-    for(let i = 0; i < num; i++) {
-       tempSum += arr[i];
-    }
-    maxSum = tempSum;
+  for (let i = 0; i < num; i++) {
+    tempSum += arr[i];
+  }
+  maxSum = tempSum;
 
-    for(let i = num; i < arr.length; i++) {
-       tempSum = tempSum - arr[i- num] + arr[i];
-       maxSum = Math.max(tempSum, maxSum);
-       }      
-       return maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(tempSum, maxSum);
+  }
+  return maxSum;
 }
 
 let array = [16, 12, 9, 19, 11, 8];
 let number = 3;
 
-console.log(maxSumArr(array, number))
+console.log(maxSumArr(array, number));
 
 let arrayTwo = [12, 24, 16, 6, 42, 50];
 let numberTwo = 2;
 
-console.log(maxSumArr(arrayTwo, numberTwo))
-
+console.log(maxSumArr(arrayTwo, numberTwo));
